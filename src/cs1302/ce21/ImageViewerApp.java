9be9630e7df6e9ebd81ace3e53d77aa86ce8ea06
Brad Barnes <bjb211@uga.edu>
2019-03-20 16:24:35 -0400

    updated screenshot and created a handler method

--- src/cs1302/ce21/ImageViewerApp.java
+++ src/cs1302/ce21/ImageViewerApp.java
@@ -20,6 +20,7 @@ import javafx.scene.layout.Priority;
  */
 public class ImageViewerApp extends Application {
 
+    Stage stage;
     Scene scene;
 
     /** The root container for the application scene graph */
@@ -47,7 +48,8 @@ public class ImageViewerApp extends Application {
      * @param stage A reference to the stage object (window) created by the system.
      */ 
     public void start(Stage stage) {
-
+        this.stage = stage;
+        
         // Initializing the nodes for the scene graph
         vbox = new VBox();
         urlLayer = new HBox(10);
@@ -61,24 +63,15 @@ public class ImageViewerApp extends Application {
         HBox.setHgrow(urlField, Priority.ALWAYS);
 
         // Load the default image with the default dimensions
-        Image img = new Image(DEFAULT_IMG, DEF_HEIGHT , DEF_WIDTH, false, false);
+        Image img = new Image(DEFAULT_IMG, DEF_HEIGHT, DEF_WIDTH, false, false);
 
         // Add the image to its container and preserve the aspect ratio if resized
         imgView = new ImageView(img);
         imgView.setPreserveRatio(true);
 
-        // EventHandler for our button. This reads the url from the textfield and loads
-        // the corresponding image into our imageview
-        EventHandler<ActionEvent> loadImgHandler = e -> {
-            try {
-                Image newImg = new Image(urlField.getText(), DEF_HEIGHT, DEF_WIDTH, false, false);
-                imgView.setImage(newImg);
-                stage.sizeToScene();
-            } catch(IllegalArgumentException iae) {
-                System.out.println("The supplied URL is invalid");
-            } // try
-        };             
-        loadImage.setOnAction(loadImgHandler);
+        // EventHandler for our button using a fancy method reference.
+        //EventHandler<ActionEvent> loadImgHandler = this::loadImage;
+        loadImage.setOnAction(this::loadImage);
 
         // Add the hbox and imageview to the containing vbox and set the vbox
         // to be the root of the scene
@@ -86,12 +79,30 @@ public class ImageViewerApp extends Application {
         scene = new Scene(vbox);
 
         // Set up the stage and set it to be visible
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.setTitle("1302 Image Viewer!");
         stage.sizeToScene();
         stage.show();
         
-    } // main
+    } // start
 
+    /**
+     * Students will provide javadoc comments here.
+     *
+     * @param e source event
+     */ 
+    private void loadImage(ActionEvent e) {
+
+        try {
+            Image newImg = new Image(urlField.getText(), DEF_HEIGHT, DEF_WIDTH, false, false);
+            imgView.setImage(newImg);
+            stage.sizeToScene();
+        } catch(IllegalArgumentException iae) {
+            System.out.println("The supplied URL is invalid");
+        } // try
+        
+    } // loadImage
+    
 } // ImageViewerApp
 
